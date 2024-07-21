3ac1ebad7a81bf24335fe278f208cfe836dd5f33
khushi280 <114170900+khushi280@users.noreply.github.com>
2024-07-21 12:33:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,42 @@
-import express from "express"
-import mongoose from "mongoose"
-import cors from "cors"
-import dotenv from "dotenv"
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import userRouter from "./routes/userRoute.js"
-import taskRouter from "./routes/taskRoute.js"
-import forgotPasswordRouter from "./routes/forgotPassword.js"
+import userRouter from "./routes/userRoute.js";
+import taskRouter from "./routes/taskRoute.js";
+import forgotPasswordRouter from "./routes/forgotPassword.js";
 
 //app config
-dotenv.config()
-const app = express()
-const port = process.env.PORT || 8001
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8001;
 mongoose.set('strictQuery', true);
 
 //middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //db config
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
 }, (err) => {
     if (err) {
-        console.log(err)
+        console.log(err);
     } else {
-        console.log("DB Connected")
+        console.log("DB Connected");
     }
-})
+});
 
 //api endpoints
-app.use("/api/user", userRouter)
-app.use("/api/task", taskRouter)
-app.use("/api/forgotPassword", forgotPasswordRouter)
+app.use("/api/user", userRouter);
+app.use("/api/task", taskRouter);
+app.use("/api/forgotPassword", forgotPasswordRouter);
+
+// root route
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
 
 //listen
-app.listen(port, () => console.log(`Listening on localhost:${port}`))
+app.listen(port, () => console.log(`Listening on localhost:${port}`));

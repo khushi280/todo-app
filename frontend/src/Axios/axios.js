62fb6ad585e984835b959f47e6779bebab9e7ed4
khushi280 <114170900+khushi280@users.noreply.github.com>
2024-07-21 13:30:03 +0530

    Update axios.js

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"https://todo-app-1-ko3s.onrender.com/"
+    baseURL:"https://todo-app-1-ko3s.onrender.com/api"
 })
 export default instance
